9bc9567a632bab6aeb8e20a389abd8af066226ec
gordonzhang <gordonzhang81@gmail.com>
2018-11-29 23:55:13 -0500

    update

--- Program.py
+++ Program.py
@@ -1,8 +1,3 @@
-"""
-Copyright (C) 2018 Interactive Brokers LLC. All rights reserved. This code is subject to the terms
-and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable.
-"""
-
 import argparse
 import datetime
 import collections
